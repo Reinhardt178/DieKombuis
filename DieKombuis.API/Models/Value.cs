cca390528c29dc27da4d5e3ccb7367c9fcb5dbee
Reinhardt <miller.reinhardt123@gmail.com>
2020-02-18 10:09:04 +0200

    Initial Commit

---
+++ DieKombuis.API/Models/Value.cs
@@ -0,0 +1,10 @@
+namespace DieKombuis.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
