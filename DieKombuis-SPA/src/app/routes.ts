1e2de8d7168a3f04cda19db541cbfcc6acaa6f0d
Reinhardt <miller.reinhardt123@gmail.com>
2020-02-26 15:52:13 +0200

    section 7

---
+++ DieKombuis-SPA/src/app/routes.ts
@@ -0,0 +1,20 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuListComponent } from './menu-list/menu-list.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children : [
+            { path: 'menu', component: MenuListComponent},
+            { path: 'orders', component: OrdersComponent},
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'}
+
+]
